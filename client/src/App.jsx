ed4b1ecf27a53a2f09c1ee237ca801cbb21dbf61
kohdc <kohdc1723@gmail.com>
2023-01-28 17:23:46 -0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+
+const App = () => {
+	return (
+		<div className="text-3xl font-bold underline">App</div>
+	);
+}
+
+export default App;
