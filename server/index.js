daad0f01b8972bb0894a0fd24fd57c1ea123051d
kohdc <kohdc1723@gmail.com>
2023-02-01 01:56:21 -0800

    on create page server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 import express from "express";
-import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./mongodb/connect.js";
-
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
     res.send("Hello");
 });
 
-const connectExpress = (port) => {
+const connectServer = (port) => {
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
@@ -22,7 +25,7 @@ const connectExpress = (port) => {
 const startServer = async () => {
     try {
         connectDb(process.env.MONGODB_CONNECTION_URI);
-        connectExpress(process.env.PORT_NUMBER);
+        connectServer(process.env.PORT_NUMBER);
     } catch (error) {
         console.error(error);
     }
